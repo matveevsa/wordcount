32ff0984bf7eb29447cd21acb7bf084d92de61f9
Matveev Sergey <s.matveev@notamedia.ru>
2022-11-07 22:15:42 +0300

    empty str

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 	if 1 >= len(os.Args) {
 		fmt.Println(0)
 	} else {
-		fmt.Println(len(strings.Split(os.Args[1], " ")))
+		str := strings.Trim(os.Args[1], "")
+
+		if len(str) > 0 {
+			fmt.Println(len(strings.Split(str, " ")))
+		} else {
+			fmt.Println(len(str))
+		}
 	}
 }
